7e0065f9d4b0b8818f758b96d89bf385a9b0e4e9
Purnima <purnima.om.gurjar@gmail.com>
2023-05-30 00:15:46 +1000

    Updates

---
+++ precache-manifest.46328b56c2af71589ac964d78045ed26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cf20ed739c21872fbe9",
+    "url": "/react-hook-context-crud/static/css/main.f4b7f419.chunk.css"
+  },
+  {
+    "revision": "6cf20ed739c21872fbe9",
+    "url": "/react-hook-context-crud/static/js/main.462d9653.chunk.js"
+  },
+  {
+    "revision": "a061db6bfc1a0638c50d",
+    "url": "/react-hook-context-crud/static/js/runtime~main.d255288e.js"
+  },
+  {
+    "revision": "aaaee42052f15b8cb919",
+    "url": "/react-hook-context-crud/static/css/2.99e2afad.chunk.css"
+  },
+  {
+    "revision": "aaaee42052f15b8cb919",
+    "url": "/react-hook-context-crud/static/js/2.5b2cb1a3.chunk.js"
+  },
+  {
+    "revision": "e4f7e59f8cf03c2f2b253e867af7bebf",
+    "url": "/react-hook-context-crud/index.html"
+  }
+];
